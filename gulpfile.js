e9fa62d347bcbb28c3440de99c768d5868eb4eca
Yoeran <contact@yoeran.nl>
2015-03-02 11:37:01 +0100

    Add inline comments. Add vendor directory.

--- gulpfile.js
+++ gulpfile.js
@@ -19,19 +19,33 @@ livereload({
 var config = {
   styles: './source/styles/',
   scripts: './source/scripts/',
+  vendor_scripts: './source/vendor/*.js',
+  vendor_css: './source/vendor/*.css',
   styles_dist: './public/assets/styles/',
   scripts_dist: './public/assets/scripts/'
 };
 
+//
+// Check yo self before yo wreck yo self
+//
 gulp.task('lint', function() {
   return gulp.src( config.scripts + '**/*.js')
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
 });
 
+//
+// Find vendors scripts (bower + vendor dir) and concatenate them into a vendor.js
+//
 gulp.task('bower', function(){
-  var packages = wiredep();
-  return gulp.src( packages.js )
+  // Get Bower dependencies
+  var packages  = wiredep(),
+      js_files  = packages.js;
+
+  // Add *.js from vendor directory
+  js_files.push( config.vendor_scripts );
+
+  return gulp.src( js_files )
     .pipe( concat('vendor.js') )
     .pipe( gulp.dest( config.scripts_dist ) )
     .pipe( rename('vendor.min.js') )
@@ -40,24 +54,34 @@ gulp.task('bower', function(){
     .pipe(livereload());
 });
 
+//
+// Concatenate all scripts and build two versions: normal and minified
+//
 gulp.task('scripts', function(){
   return gulp.src( config.scripts + '*.js' )
+    // normal version
     .pipe( concat('main.js') )
     .pipe( gulp.dest( config.scripts_dist ) )
+    // minified version
     .pipe( rename('main.min.js') )
     .pipe( uglify() )
     .pipe( gulp.dest( config.scripts_dist ) )
     .pipe(livereload());
 });
 
+//
+// Compile using SASS, add Autoprefixer and build two versions: normal and minified
+//
 gulp.task('scss', function () {
   return gulp.src( config.styles + 'main.scss' )
+    // normal version
     .pipe(sass())
     .pipe(autoprefixer({
       browsers: ['> 5%', 'last 2 versions'],
       cascade: false,
     }))
     .pipe(gulp.dest( config.styles_dist ))
+    // minified version
     .pipe( rename('main.min.css') )
     .pipe(minifyCSS())
     .pipe(gulp.dest( config.styles_dist ))
@@ -67,9 +91,8 @@ gulp.task('scss', function () {
 gulp.task('watch', function(){
   gulp.watch( config.styles + '**/*.scss', ['scss'] );
   gulp.watch( config.scripts + '**/*.js', ['lint','scripts']);
-  gulp.watch( './bower.json', ['bower']);
+  gulp.watch( './bower.json', ['bower']); // only run the bower task when something has changed, costly operation otherwise
 });
 
 gulp.task('build', ['lint','scss','scripts','bower']);
-gulp.task('serve', ['build','watch']);
-gulp.task('default', ['build']);
+gulp.task('default', ['build','watch']);
